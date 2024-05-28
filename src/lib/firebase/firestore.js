8cb66ff40919edcfaa50471c3e46e53dfbe65e17
martin <martin@fabbri.ai>
2024-05-28 05:45:21 +0000

    Read the list of restaurants from Firestore

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -56,21 +56,6 @@ export async function getRestaurants(db = db, filters = {}) {
 	});
 }
 
-export async function getRestaurants(db = db, filters = {}) {
-	let q = query(collection(db, "restaurants"));
-
-	q = applyQueryFilters(q, filters);
-	const results = await getDocs(q);
-	return results.docs.map(doc => {
-			return {
-					id: doc.id,
-					...doc.data(),
-					// Only plain objects can be passed to Client Components from Server Components
-					timestamp: doc.data().timestamp.toDate(),
-			};
-	});
-}
-
 export function getRestaurantsSnapshot(cb, filters = {}) {
 	return;
 }
